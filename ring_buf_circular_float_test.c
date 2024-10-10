ed81535113751c156f09a057935cc2e4b6a1709a
Roy Ratcliffe <roy@ratcliffe.me>
2024-10-10 18:02:04 +0100

    test: circular float

---
+++ ring_buf_circular_float_test.c
@@ -0,0 +1,39 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ring_buf.h"
+
+/*
+ * Use \c ring_buf_get rather than \c ring_buf_get_claim, but the former handles
+ * discontiguous items. They amount to the same thing when the buffer size is a
+ * multiple of the item size.
+ */
+int put_circular_float(struct ring_buf *buf, float number) {
+  if (ring_buf_is_full(buf))
+    (void)ring_buf_get_ack(buf, ring_buf_get(buf, NULL, sizeof(number)));
+  if (sizeof(number) > ring_buf_free(buf))
+    return -EMSGSIZE;
+  return ring_buf_put_ack(buf, ring_buf_put(buf, &number, sizeof(number)));
+}
+
+int ring_buf_circular_float_test(int argc, char **argv) {
+  RING_BUF_DECLARE(buf, sizeof(float[2U]));
+  for (float number = 1.0F; number <= 10.0F; number += 1.0F) {
+    int err = put_circular_float(&buf, number);
+    assert(err == 0);
+  }
+  float sum = 0.0F, number;
+  ring_buf_size_t ack;
+  while ((ack = ring_buf_get(&buf, &number, sizeof(number))))
+    sum += number;
+  assert(sum == 9.0F + 10.0F);
+  ring_buf_get_ack(&buf, 0U);
+  while ((ack = ring_buf_get(&buf, &number, sizeof(number)))) {
+    int err = ring_buf_get_ack(&buf, ack);
+    assert(err == 0);
+    printf("%f\n", number);
+  }
+
+  return EXIT_SUCCESS;
+}
