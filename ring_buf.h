e84fd05050eb61a2b4a4abc088dd690548b9cdf0
Roy Ratcliffe <roy@ratcliffe.me>
2024-10-10 14:31:23 +0100

    docs: doxygen

--- ring_buf.h
+++ ring_buf.h
@@ -1,3 +1,27 @@
+/*!
+ * \file ring_buf.h
+ * \copyright Roy Ratcliffe, Northumberland, United Kingdom
+ *
+ * Permission is hereby granted, free of charge,  to any person obtaining a
+ * copy  of  this  software  and    associated   documentation  files  (the
+ * "Software"), to deal in  the   Software  without  restriction, including
+ * without limitation the rights to  use,   copy,  modify,  merge, publish,
+ * distribute, sublicense, and/or sell  copies  of   the  Software,  and to
+ * permit persons to whom the Software is   furnished  to do so, subject to
+ * the following conditions:
+ *
+ *     The above copyright notice and this permission notice shall be
+ *     included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT  WARRANTY OF ANY KIND, EXPRESS
+ * OR  IMPLIED,  INCLUDING  BUT  NOT   LIMITED    TO   THE   WARRANTIES  OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR   PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS  OR   COPYRIGHT  HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY,  WHETHER   IN  AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM,  OUT  OF   OR  IN  CONNECTION  WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #pragma once
 
 #include <errno.h>
@@ -55,6 +79,15 @@ ring_buf_size_t ring_buf_put_claim(struct ring_buf *buf, void **data, ring_buf_s
 
 int ring_buf_put_ack(struct ring_buf *buf, ring_buf_size_t size);
 
+/*!
+ * \brief Puts non-contiguous bytes into the ring buffer.
+ * \details The return value may be less than the given size if the buffer runs
+ * out of free space.
+ * \param buf Ring buffer.
+ * \param data Address of bytes to put.
+ * \param size Number of bytes to put.
+ * \returns Buffer space to acknowledge in bytes.
+ */
 ring_buf_size_t ring_buf_put(struct ring_buf *buf, const void *data, ring_buf_size_t size);
 
 /*!
