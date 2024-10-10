d3e43b99c3610f52f42d58a2df67060d3f594e63
Roy Ratcliffe <roy@ratcliffe.me>
2024-10-10 18:35:36 +0100

    docs: more doxygen

--- ring_buf.h
+++ ring_buf.h
@@ -27,8 +27,8 @@
 #pragma once
 
 #include <errno.h>
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
 
 typedef ptrdiff_t ring_buf_ptrdiff_t;
 
@@ -85,31 +85,52 @@ static inline bool ring_buf_is_full(const struct ring_buf *buf) {
 }
 
 /*!
+ * \brief Claims space for putting data into a ring buffer.
  * \details Claims contiguous space. Advances the "put" head.
  */
-ring_buf_size_t ring_buf_put_claim(struct ring_buf *buf, void **data, ring_buf_size_t size);
+ring_buf_size_t ring_buf_put_claim(struct ring_buf *buf, void **data,
+                                   ring_buf_size_t size);
 
+/*!
+ * \brief Acknowledges space claimed for putting data into a ring buffer.
+ * \param buf Ring buffer address.
+ * \param size Number of bytes to acknowledge.
+ * \returns 0 Successful put.
+ * \returns -EINVAL Size exceeds previously claimed aggregate space.
+ */
 int ring_buf_put_ack(struct ring_buf *buf, ring_buf_size_t size);
 
 /*!
  * \brief Puts non-contiguous bytes into the ring buffer.
  * \details The return value may be less than the given size if the buffer runs
  * out of free space.
+ * \note Does \e not automatically acknowledge the space.
  * \param buf Ring buffer.
  * \param data Address of bytes to put.
  * \param size Number of bytes to put.
  * \returns Buffer space to acknowledge in bytes.
  */
-ring_buf_size_t ring_buf_put(struct ring_buf *buf, const void *data, ring_buf_size_t size);
+ring_buf_size_t ring_buf_put(struct ring_buf *buf, const void *data,
+                             ring_buf_size_t size);
 
 /*!
+ * \brief Claims contiguous bytes for getting.
  * \details Advances the "get" head.
  */
-ring_buf_size_t ring_buf_get_claim(struct ring_buf *buf, void **data, ring_buf_size_t size);
+ring_buf_size_t ring_buf_get_claim(struct ring_buf *buf, void **data,
+                                   ring_buf_size_t size);
 
 int ring_buf_get_ack(struct ring_buf *buf, ring_buf_size_t size);
 
-ring_buf_size_t ring_buf_get(struct ring_buf *buf, void *data, ring_buf_size_t size);
+/*!
+ * \brief Gets data from a ring buffer.
+ * \details Copies discontinuous data.
+ * \param data Address of copied data, or \c NULL to ignore.
+ * \param size Number of bytes to get.
+ * \returns Number of bytes to acknowledge.
+ */
+ring_buf_size_t ring_buf_get(struct ring_buf *buf, void *data,
+                             ring_buf_size_t size);
 
 #include <stdint.h>
 
