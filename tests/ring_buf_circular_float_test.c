16e07a27f2924c0121c2ee255dc3bc90dd73688f
Roy Ratcliffe <roy@ratcliffe.me>
2025-10-31 08:30:23 +0000

    style: improve comments for clarity in ring_buf_circular_float_test.c

--- tests/ring_buf_circular_float_test.c
+++ tests/ring_buf_circular_float_test.c
@@ -19,7 +19,17 @@ int put_circular_float(struct ring_buf *buf, float number) {
 
 int ring_buf_circular_float_test(int argc, char **argv) {
   RING_BUF_DEFINE(buf, sizeof(float[2U]));
+
+  /*
+   * Reset the buffer to a size just below the maximum. This forces the buffer
+   * to wrap around when putting data.
+   */
   ring_buf_reset(&buf, RING_BUF_SIZE_MAX - 1);
+
+  /*
+   * Fill the buffer with ten floats, causing the first eight to be overwritten.
+   * Then read back the floats and verify that only the last two remain.
+   */
   for (float number = 1.0F; number <= 10.0F; number += 1.0F) {
     int err = put_circular_float(&buf, number);
     assert(err == 0);
